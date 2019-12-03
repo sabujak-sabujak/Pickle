2dc16b4310ebf132f7c96d6335f1d82fb6fc8d06
Soohwan <soohwan.ok@navercorp.com>
2019-12-03 23:02:25 +0900

    Insta와 Pickle 기본 프레그먼트로 분리

---
+++ pickle/src/main/java/life/sabujak/pickle/ui/basic/PickleFragment.kt
@@ -0,0 +1,57 @@
+package life.sabujak.pickle.ui.basic
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
+import life.sabujak.pickle.R
+import life.sabujak.pickle.databinding.FragmentInstaBinding
+import life.sabujak.pickle.ui.PickleViewModel
+import life.sabujak.pickle.ui.insta.InstaAdapter
+import life.sabujak.pickle.util.Logger
+
+
+class PickleFragment : Fragment() {
+
+    val logger = Logger.getLogger(PickleFragment::class.java.simpleName)
+
+    lateinit var binding: FragmentInstaBinding
+    lateinit var viewModel: PickleViewModel
+    val adapter = InstaAdapter()
+    val gridLayoutManager by lazy {
+        GridLayoutManager(context, 3)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        activity?.let {
+            viewModel = ViewModelProviders.of(it).get(PickleViewModel::class.java)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        logger.d("onCreate")
+        viewModel.items.observe(this, Observer { pagedList ->
+            adapter.submitList(pagedList)
+        })
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_insta, container, false)
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = gridLayoutManager
+        return binding.root
+    }
+
+}
--- sample/src/main/java/life/sabujak/picklesample/MainActivity.kt
+++ sample/src/main/java/life/sabujak/picklesample/MainActivity.kt
@@ -3,12 +3,20 @@ package life.sabujak.picklesample
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import life.sabujak.pickle.ui.PickleActivity
+import life.sabujak.picklesample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, PickleActivity::class.java))
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        //Pickle을 어떤식으로 호출할지 고민중
+        //임시로 임의의 action을 정의함
+        binding.insta.setOnClickListener { startActivity(Intent(this, PickleActivity::class.java).apply { action="insta" }) }
+        binding.basic.setOnClickListener { startActivity(Intent(this, PickleActivity::class.java).apply { action="basic" }) }
+
     }
 }
