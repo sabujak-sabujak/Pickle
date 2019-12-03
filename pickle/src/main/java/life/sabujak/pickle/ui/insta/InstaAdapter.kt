9bd01fef1a31e1cd845f8044e2595a07a38ec4aa
kklyoon <kklyoon@gmail.com>
2019-12-03 18:02:09 +0900

    인스타스타일 레이아웃, 이미지 프리뷰, scaletype 변경 버튼

--- pickle/src/main/java/life/sabujak/pickle/ui/insta/InstaAdapter.kt
+++ pickle/src/main/java/life/sabujak/pickle/ui/insta/InstaAdapter.kt
@@ -1,13 +1,23 @@
 package life.sabujak.pickle.ui.insta
 
+import android.view.View
 import android.view.ViewGroup
 import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.DiffUtil
+import kotlinx.android.synthetic.main.view_insta_media.view.*
 import life.sabujak.pickle.data.entity.PickleMedia
 import life.sabujak.pickle.ui.BindingHolder
+import life.sabujak.pickle.util.Logger
 
 class InstaAdapter : PagedListAdapter<PickleMedia, BindingHolder>(diffCallback) {
 
+    val logger = Logger.getLogger(InstaAdapter::class)
+
+    interface ItemClick {
+        fun onClick(view: View, position: Int)
+    }
+
+    var itemClick: ItemClick? = null
 
     companion object {
         val diffCallback = object : DiffUtil.ItemCallback<PickleMedia>() {
@@ -35,8 +45,16 @@ class InstaAdapter : PagedListAdapter<PickleMedia, BindingHolder>(diffCallback) 
         val item = getItem(position)
         item?.let {
             holder.binding.setVariable(item.getType().variableId,item)
+            holder.itemView.setOnClickListener{v->
+                logger.d("item Clicked")
+//                holder.itemView.v_cover.visibility = View.VISIBLE
+                itemClick?.onClick(v, position)
+            }
         }
 
     }
 
+    fun getPickleMeida(position: Int): PickleMedia?{
+        return getItem(position)
+    }
 }
--- pickle/src/main/java/life/sabujak/pickle/ui/insta/InstaFragment.kt
+++ pickle/src/main/java/life/sabujak/pickle/ui/insta/InstaFragment.kt
@@ -5,10 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.fragment_insta.*
 import life.sabujak.pickle.R
 import life.sabujak.pickle.databinding.FragmentInstaBinding
 import life.sabujak.pickle.util.Logger
@@ -21,8 +24,10 @@ class InstaFragment : Fragment() {
     lateinit var viewModel :InstaViewModel
     val adapter = InstaAdapter()
     val gridLayoutManager by lazy {
-        GridLayoutManager(context, 3)
+        GridLayoutManager(context, 4)
     }
+
+    var selectedPosition: Int = -1
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -43,14 +48,34 @@ class InstaFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_insta, container, false)
+        binding.frag = this
         binding.recyclerView.adapter = adapter
         binding.recyclerView.layoutManager = gridLayoutManager
+        adapter.itemClick = object: InstaAdapter.ItemClick{
+            override fun onClick(view: View, position: Int) {
+                selectedPosition = position
+                loadImageView(position)
+            }
+        }
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
     }
+
+    fun loadImageView(position: Int){
+        val selected = adapter.getPickleMeida(position)
+        selected?.getUri()?.let{
+            Glide.with(iv_preview).load(it).into(iv_preview)
+        }
+    }
+
+    fun ratioBtnClicked(view: View){
+        logger.d("ratioBtnClicked")
+        if(iv_preview.scaleType == ImageView.ScaleType.CENTER_CROP) iv_preview.scaleType = ImageView.ScaleType.CENTER_INSIDE
+        else iv_preview.scaleType = ImageView.ScaleType.CENTER_CROP
+        loadImageView(selectedPosition)
+    }
+
 }
